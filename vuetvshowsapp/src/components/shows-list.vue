69a15add6687b7b02d24d87a1a9d1f879a683d79
MKONAJE <manjunath_40@yahoo.co.in>
2022-07-19 21:34:27 +0200

    reusable shows component

--- vuetvshowsapp/src/components/shows-list.vue
+++ vuetvshowsapp/src/components/shows-list.vue
@@ -5,12 +5,13 @@
                  :is-full-page="fullPage">
         </loading>
         <div class="shows row col-12" v-if="tvShowKey != ''">
-            <div style="color: #2aeb74;" class="card col-3 p-4 m-4 bg-dark" v-for="tvshow in showsData" :key="tvshow.show.id">
+            <div style="color: #2aeb74;" class="card col-3 p-4 m-4 bg-dark align-items-centre" v-for="tvshow in showsData" :key="tvshow.show.id">
                 <ShowsComponent :name="tvshow.show.name"
                                 :language="tvshow.show.language"
                                 :rating="tvshow.show.rating.average"
                                 :id="tvshow.show.id"
-                                :showKey="tvshow.show.name"></ShowsComponent>
+                                :showKey="tvshow.show.name"
+                                :image="tvshow.show.image"></ShowsComponent>
              </div>
         </div>
             <div class="shows row col-12" v-else>
@@ -19,7 +20,8 @@
                                     :language="tvshow.language"
                                     :rating="tvshow.rating.average"
                                     :id="tvshow.id"
-                                    :showKey="tvshow.name"></ShowsComponent>
+                                    :showKey="tvshow.name"
+                                    :image="tvshow.image"></ShowsComponent>
                 </div>
             </div>
         </div>
@@ -52,3 +54,11 @@
         }
     }
 </script>
+<style>
+    .topic-thumbnail {
+        margin-right: 15px;
+        width: 36%;
+        height: 85%;
+        overflow: hidden;
+    }
+</style>
--- vuetvshowsapp/src/components/shows.vue
+++ vuetvshowsapp/src/components/shows.vue
@@ -1,14 +1,17 @@
 <template>
-        <h2>{{ name }}</h2>
-        <p>({{ language }})</p>
-        <h4>
-            Rating :
-            <span v-if="average">{{average }}</span>
-            <span v-else>NA</span>
-        </h4>
-        <router-link :to="`/tvshow/${id}/${showKey}`">
-            <button class="py-2 px-3">View Tv Show</button>
-        </router-link>
+    <h2>{{ name }}</h2>
+    <p>({{ language }})</p>
+    <h4>
+        Rating :
+        <span v-if="rating">{{rating }}</span>
+        <span v-else>NA</span>
+    </h4>
+    <img v-if="image" class="topic-thumbnail my-4"
+         alt="tvshow"
+         :src="image.original" />
+    <router-link :to="`/tvshow/${id}/${showKey}`">
+        <button class="py-2 px-3">View Tv Show</button>
+    </router-link>
 </template>
 <script>
     export default {
@@ -28,6 +31,9 @@
             },
             showKey: {
                 type: String,
+            },
+            image: {
+                type: Object,
             },
         },
     };
