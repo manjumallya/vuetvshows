21eb625a87cc086c6fac6e790dc74536362bda34
MKONAJE <manjunath_40@yahoo.co.in>
2022-07-19 11:20:44 +0200

    refactoring css and using bootstrap css

--- vuetvshowsapp/src/views/TvShow.vue
+++ vuetvshowsapp/src/views/TvShow.vue
@@ -6,20 +6,25 @@
 		
 		<div class="details p-2 bg-dark mt-4 rounded-3">
 			<h3>Details</h3>
-			<div class="d-flex flex-column align-items-end">
-				<img v-if="tvshow.show.image" class="topic-thumbnail"
-					 alt="tvshow"
-					 :src="tvshow.show.image.original" />
-			</div>
-			<h4><a :href="tvshow.show.url" target="_blank">Show info</a></h4>
-			<h4>Premiere:</h4>
-			<h5>start: {{ tvshow.show.premiered}}</h5>
-			<h5>end: {{ tvshow.show.ended}}</h5>
-			<br />
-			<h4 v-if="tvshow.show.schedule">schedule:</h4>
-			<h5 v-if="tvshow.show.schedule.time">time: {{ tvshow.show.schedule.time}}</h5>
-			<div v-for="day in tvshow.show.schedule.days" :key="day">
-				<h5>{{ day}}</h5>
+			<div class="row">
+				<div class="col-6">
+					<h4><a :href="tvshow.show.url" target="_blank">Show info</a></h4>
+					<h4>Premiere:</h4>
+					<h5>start: {{ tvshow.show.premiered}}</h5>
+					<h5>end: {{ tvshow.show.ended}}</h5>
+					<br />
+					<h4 v-if="tvshow.show.schedule">schedule:</h4>
+					<h5 v-if="tvshow.show.schedule.time">time: {{ tvshow.show.schedule.time}}</h5>
+					<div v-for="day in tvshow.show.schedule.days" :key="day">
+						<h5>{{ day}}</h5>
+					</div>
+				</div>
+				<div class="col-6 d-flex flex-column align-items-end">
+					<img v-if="tvshow.show.image" class="topic-thumbnail"
+						 alt="tvshow"
+						 :src="tvshow.show.image.original" />
+				</div>
+
 			</div>
 			<hr /> 
 			<h5 class="mt-4" v-html="tvshow.show.summary"></h5>
@@ -72,7 +77,8 @@ export default {
 
 .topic-thumbnail {
   margin-right: 15px;
-  width: 10%;
+  width: 36%;
+  height: 85%;
   overflow: hidden;
 }
 
